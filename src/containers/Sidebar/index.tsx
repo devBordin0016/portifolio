f9993c4fd0673904b1ba4ca251cc1aa56337a564
Fernando Bordin <fernandobordin07@hotmail.com>
2024-09-23 19:45:46 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/devBordin0016.png" />
-    <Titulo fontSize={20}>Fernando Bordin</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Fernando Bordin</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Dev Bordin
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Fron-End
+      </Descricao>
+      <BotaoTema>Troca Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
