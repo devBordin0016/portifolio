097e64075e0dcd66508e1e89783112c1ef1ad6cb
Fernando Bordin <fernandobordin07@hotmail.com>
2024-09-23 18:46:20 -0300

    construindo a estrutura e a secao sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/devBordin0016.png" />
+    <Titulo fontSize={20}>Fernando Bordin</Titulo>
+  </aside>
+)
+
+export default Sidebar
